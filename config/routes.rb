0e5e89136174cdf448b95b8251082c4a646e73a0
Tony Mamona <testemailsth@testgmail.com>
2018-09-01 18:48:27 +0000

    create new templet action

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
